8f430b7c69e9fa716d86bb4f7d6b5f47c1c64eea
yyueqqi <wandonnaya@gmail.com>
2023-11-07 22:08:55 +0700

    wan

--- website/artworkpage.php
+++ website/artworkpage.php
@@ -1,3 +1,21 @@
+<?php
+
+    include '../function/config.php';
+
+    $sql = "SELECT * FROM artwork";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $artworks = $result->fetch_all(MYSQLI_ASSOC);
+    } else {
+        $artworks = array(); 
+    }
+
+    echo json_encode($artworks);
+
+    $conn->close();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -69,6 +87,58 @@
             </div>
         </section>
     </main>
+
+    <script>
+    // Function to fetch and display artwork
+    function fetchArtwork() {
+        // Fetch artwork data from the PHP script
+        fetch('fetch_artwork.php')
+            .then(response => response.json())
+            .then(data => {
+                // Code to display artwork data
+                const artworkContainer = document.querySelector('.artwork');
+                artworkContainer.innerHTML = '';
+
+                data.forEach(artwork => {
+                    const artPiece = document.createElement('div');
+                    artPiece.className = 'art-piece';
+
+                    // Create elements for artwork details
+                    const img = document.createElement('img');
+                    img.src = artwork.image_url;
+                    img.alt = artwork.title;
+
+                    const title = document.createElement('h3');
+                    title.textContent = artwork.title;
+
+                    const artist = document.createElement('p');
+                    artist.textContent = 'Artist: ' + artwork.artist;
+
+                    const price = document.createElement('p');
+                    price.textContent = 'Price: $' + artwork.price;
+
+                    // Create a Buy Now button or any other action you need
+                    const buyButton = document.createElement('button');
+                    buyButton.className = 'buy-button';
+                    buyButton.textContent = 'Buy Now';
+
+                    // Append elements to the art piece
+                    artPiece.appendChild(img);
+                    artPiece.appendChild(title);
+                    artPiece.appendChild(artist);
+                    artPiece.appendChild(price);
+                    artPiece.appendChild(buyButton);
+
+                    // Append the art piece to the artwork container
+                    artworkContainer.appendChild(artPiece);
+                });
+            })
+            .catch(error => console.error('Error fetching artwork:', error));
+    }
+
+    // Call the fetchArtwork function to load artwork when the page loads
+    fetchArtwork();
+</script>
     
 </body>
 </html>
