9ba5d035fd6603daef9082c22d51ddaed44804fe
yyueqqi <wandonnaya@gmail.com>
2023-11-15 22:19:55 +0700

    wan

--- function/addtocart.php
+++ function/addtocart.php
@@ -3,24 +3,25 @@ session_start();
 
 include 'config.php';
 
-    if (isset($_GET['add_to_cart_artwork']))
+    if (isset($_GET['add_to_cart_artwork'])){
     $account_username = $_SESSION['login_username'];
     $artwork_id = $_GET['add_to_cart'];
     $quantity = 1;
-    $select = "SELECT * FROM 'cart' where username = '$account_usernam' and item_id = '$artwork_id'";
+    $select = "SELECT * FROM 'cart' where username = '$account_username' and item_id = '$artwork_id'";
     $result = mysqli_query($conn,$select);
     $num_row = mysqli_num_rows($result);
         if($num_row > 0) {
         echo '<script>alert("already have in CART")</script>';
-        echo '<script>window.open("index.php","_self")</script>';
+        echo '<script>window.open("artworkpage.php","_self")</script>';
+        }
         else {
-        $insert = "INSERT INTO'cart'(username, item_id, quantity) VALUES ('$username', '$artwork_id', '$quantity')";
+        $insert = "INSERT INTO'cart'(username, item_id, quantity) VALUES ('$account_username', '$artwork_id', '$quantity')";
         $result_query = mysqli_query($conn,$insert);
-        echo '<script>window.open("index.php","_self")</script>';
+        echo '<script>window.open("artworkpage.php","_self")</script>';
         }
     }
 
-    if (isset($_GET['add_to_cart_ticket']))
+    if (isset($_GET['add_to_cart_ticket'])){
     $account_username = $_SESSION['login_username'];
     $ticket_id = $_GET['add_to_cart'];
     $quantity = 1;
@@ -30,9 +31,12 @@ include 'config.php';
         if($num_row > 0) {
         echo '<script>alert("already have in CART")</script>';
         echo '<script>window.open("index.php","_self")</script>';
+        }
         else {
         $insert = "INSERT INTO'cart'(username, item_id, quantity) VALUES ('$username', '$ticket_id', '$quantity')";
         $result_query = mysqli_query($conn,$insert);
         echo '<script>window.open("index.php","_self")</script>';
         }
     }
+
+?>
