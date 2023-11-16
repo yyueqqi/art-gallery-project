6c60860d84385b7ae42adf839e007ac1ec32c785
yyueqqi <wandonnaya@gmail.com>
2023-11-17 04:26:43 +0700

    wan

--- website/cart.php
+++ website/cart.php
@@ -1,51 +1,46 @@
 <?php
+session_start();
+include '../function/config.php';
 
-    session_start();
-    include '../function/config.php';
+// Fetch cart items from the database
+$sql = "SELECT * FROM cart";
+$result = $conn->query($sql);
 
-    // Fetch cart items from the database
-    $sql = "SELECT * FROM cart";
-    $result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    $cart_items = $result->fetch_all(MYSQLI_ASSOC);
+} else {
+    $cart_items = array();
+}
 
-    if ($result->num_rows > 0) {
-        $cart_items = $result->fetch_all(MYSQLI_ASSOC);
-    } 
-    else {
-        $cart_items = array();
+$total = 0;
+foreach ($cart_items as $cart_item) {
+    // Determine the item type based on your database structure
+    $item_type = $cart_item['item_type'];
+
+    if ($item_type == "Artwork" || $item_type == "vip Ticket" || $item_type == "general Ticket" || $item_type == "student Ticket") {
+        $total += $cart_item['price'] * $cart_item['quantity'];
+    }
+}
+
+if (isset($_POST['delete_item'])) {
+    $item_id_to_delete = $_POST['item_id'];
+
+    // Perform the deletion from the database based on your table structure
+    $delete_sql = "DELETE FROM `cart` WHERE item_id = $item_id_to_delete";
+    $delete_result = $conn->query($delete_sql);
+
+    // You can add additional checks and messages based on the deletion result
+    if ($delete_result) {
+        echo '<script>alert("Item deleted successfully")</script>';
+    } else {
+        echo '<script>alert("Error deleting item")</script>';
     }
 
-    $total = 0;
-    foreach ($cart_items as $cart_item) {
-        // Determine the item type based on your database structure
-        $item_type = $cart_item['item_type'];
+    // Redirect to refresh the page after deletion
+    echo '<script>window.location.href = "cart.php";</script>';
+}
 
-        if ($item_type == "Artwork") {
-            $total += $cart_item['price'] * $cart_item['quantity'];
-        } 
-        elseif ($item_type == "vip Ticket" || $item_type == "general Ticket" || $item_type == "student Ticket") {
-            $total += $cart_item['price'] * $cart_item['quantity'];
-        }
-    }
-
-    if (isset($_POST['delete_item'])) {
-        $item_id_to_delete = $_POST['item_id'];
-        
-        // Perform the deletion from the database based on your table structure
-        $delete_sql = "DELETE FROM `cart` WHERE item_id = $item_id_to_delete";
-        $delete_result = $conn->query($delete_sql);
-        
-        // You can add additional checks and messages based on the deletion result
-        if ($delete_result) {
-            echo '<script>alert("Item deleted successfully")</script>';
-        } else {
-            echo '<script>alert("Error deleting item")</script>';
-        }
-
-        // Redirect to refresh the page after deletion
-        echo '<script>window.location.href = "cart.php";</script>';
-    }
-
-    $conn->close();
+$conn->close();
 ?>
 
 <!DOCTYPE html>
@@ -55,7 +50,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shopping Cart</title>
     <link rel="stylesheet" href="../style/cart_styles.css">
-    <script src="../script/script.js"></script>
+    
+    <script>
+        function increaseCount(a, b) {
+            var input = b.previousElementSibling;
+            var value = parseInt(input.value, 10);
+            value = isNaN(value) ? 0 : value;
+            value++;
+            input.value = value;
+            updateTotal(); // Call the updateTotal function when increasing quantity
+        }
+
+        function decreaseCount(a, b) {
+            var input = b.nextElementSibling;
+            var value = parseInt(input.value, 10);
+            if (value > 1) {
+                value = isNaN(value) ? 0 : value;
+                value--;
+                input.value = value;
+                updateTotal(); // Call the updateTotal function when decreasing quantity
+            }
+        }
+
+        function updateTotal() {
+            var totalPrice = 0;
+            var rows = document.querySelectorAll('table tr:not(:first-child)');
+
+            rows.forEach(function (row) {
+                var priceElement = row.querySelector('.price');
+                var quantityElement = row.querySelector('.counter input');
+                var totalElement = row.querySelector('.total-price'); // Updated this line
+                var price = parseFloat(priceElement.textContent.replace('฿', '').trim());
+                var quantity = parseInt(quantityElement.value);
+
+                var total = price * quantity;
+                totalElement.textContent = total.toFixed(2) + ' ฿'; // Updated this line
+
+                totalPrice += total;
+            });
+
+            document.getElementById('totalAmount').textContent = 'Total: ฿' + totalPrice.toFixed(2);
+        }
+    </script>
+
 </head>
 <body>
 
@@ -85,13 +122,13 @@
                 </td>
                 <td><?php echo $cart_item['item_title']; ?></td>
                 <td><?php echo $cart_item['item_type']; ?></td>
-                <td><?php echo "{$cart_item['price']} ฿"; ?></td>
+                <td class="price"><?php echo "{$cart_item['price']} ฿"; ?></td>
                 <td class="counter">
                     <span class="down" onClick='decreaseCount(event, this)'>-</span>
-                    <input type="text" value="<?php echo $cart_item['quantity']; ?>">
+                    <input type="text" value="<?php echo $cart_item['quantity']; ?>" onchange="updateTotal()">
                     <span class="up" onClick='increaseCount(event, this)'>+</span>
                 </td>
-                <td><?php echo $cart_item['price'] * $cart_item['quantity'] . " ฿"; ?></td>
+                <td class="total-price"><?php echo $cart_item['price'] * $cart_item['quantity'] . " ฿"; ?></td>
                 <td>
                     <form method="post">
                         <input type="hidden" name="item_id" value="<?php echo $cart_item['item_id']; ?>">
@@ -103,7 +140,7 @@
     </table>
 
     <div class="total-section">
-        <strong>Total: $<?php echo $total; ?></strong>
+        <strong id="totalAmount">Total: ฿<?php echo $total; ?></strong>
     </div>
 
     <div class="checkout-section">
@@ -113,3 +150,4 @@
 
 </body>
 </html>
+
