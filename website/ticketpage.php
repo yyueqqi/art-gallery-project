a31d6b90645b15098fc2f8a621e7412a8707b82c
ChanisW <aumaimcnk@gmail.com>
2023-11-17 23:05:59 +0700

    ticket aiim

--- website/ticketpage.php
+++ website/ticketpage.php
@@ -39,45 +39,62 @@ $conn->close();
     <?php include('header.php'); ?>
 
     <main>
-        <?php if ($exhibitionDetails) : ?>
-            <div class="ticket-details">
-                <h2>Exhibition Details</h2>
-                <p>Title: <?php echo $exhibitionDetails['exhibition_title']; ?></p>
-                <p>Date: <?php echo $exhibitionDetails['exhibition_date']; ?></p>
-                <p>Location: <?php echo $exhibitionDetails['location']; ?></p>
-                <p>Price: <?php echo $exhibitionDetails['ticket_price']; ?></p>
-            </div>
-        <?php else : ?>
-            <p>Exhibition not found.</p>
-        <?php endif; ?>
+        <div class="top">
+            <h5>Select your ticket type</h5>
+            <select name="ticketID" id="ticketID" required>
+                                <option value="" data-price="">Select Ticket</option>
+                                <?php
+                                include '../function/config.php';
 
+                                if (isset($exhibition_id)) {
+                                    $ticketQuery = "SELECT ticket_id, exhibition_id, ticket_type, price FROM ticket WHERE exhibition_id = $exhibition_id";
+                                    $ticketResult = $conn->query($ticketQuery);
 
-        <select name="ticketID" id="ticketID" required>
-            <option value="" data-price="">Select Ticket</option>
-            <?php
-            include '../function/config.php';
-
-            if (isset($exhibition_id)) {
-                $ticketQuery = "SELECT ticket_id, exhibition_id, ticket_type, price FROM ticket WHERE exhibition_id = $exhibition_id";
-                $ticketResult = $conn->query($ticketQuery);
-
-                if ($ticketResult->num_rows > 0) {
-                    while ($row = $ticketResult->fetch_assoc()) {
-                        $ticketID = $row['ticket_id'];
-                        $ticketType = $row['ticket_type'];
-                        $ticketPrice = $row['price'];
-                        echo "<option value='$ticketID' data-price='$ticketPrice'>$ticketType</option>";
-                    }
-                } else {
-                    echo "<option value='' data-price='' disabled>No tickets available</option>";
-                }
-            } else {
-                echo "<option value='' data-price='' disabled>Please select an exhibition first</option>";
-            }
-            ?>
-        </select>
-
-        <p id="ticketPriceDisplay">Ticket Price: $<span id="selectedTicketPrice">0.00</span></p>
+                                    if ($ticketResult->num_rows > 0) {
+                                        while ($row = $ticketResult->fetch_assoc()) {
+                                            $ticketID = $row['ticket_id'];
+                                            $ticketType = $row['ticket_type'];
+                                            $ticketPrice = $row['price'];
+                                            echo "<option value='$ticketID' data-price='$ticketPrice'>$ticketType</option>";
+                                        }
+                                    } else {
+                                        echo "<option value='' data-price='' disabled>No tickets available</option>";
+                                    }
+                                } else {
+                                    echo "<option value='' data-price='' disabled>Please select an exhibition first</option>";
+                                }
+                                ?>
+                            </select>
+        </div>
+        <div class="ticket-frame">
+            <?php if ($exhibitionDetails) : ?>
+                <div class="ticket-details">
+                    <h6><?php echo $exhibitionDetails['exhibition_title']; ?></h6>
+                    <img src="<?php echo $exhibitionDetails['exhibition_img']; ?>" alt="<?php echo $exhibitionDetails['exhibition_id']; ?>">
+                    <div class="td">
+                        <div class="td1">
+                            <img src="../image/calendar.png" alt="">
+                            <p>Date: <?php echo $exhibitionDetails['exhibition_date']; ?></p>
+                        </div>
+                        <div class="td2">
+                            <img src="../image/location.png" alt="">
+                            <p>Location: <?php echo $exhibitionDetails['location']; ?></p>
+                        </div>
+                        <div class="td3">
+                            <img src="../image/invoice.png" alt="">
+                            <p id="ticketPriceDisplay">Ticket Price: $<span id="selectedTicketPrice">0.00</span></p>
+                        </div>
+                        <div class="td4">
+                            <img src="../image/barcode.png" alt="">
+                            <div >
+                                <a id="addToCartLink" class="addtocart" href="#">ADD TO CART</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php else : ?>
+                <p>Exhibition not found.</p>
+            <?php endif; ?>
 
         <script>
             document.getElementById('ticketID').addEventListener('change', function() {
@@ -92,14 +109,9 @@ $conn->close();
             });
         </script>
 
-        <div style="display:flex; flex-direction:row; padding-left: 800px;">
-            <!-- Use the selected ticket ID in the link -->
-            <a id="addToCartLink" class="addtocart" href="#">ADD TO CART</a>
-        </div>
-
+    </div>
     </main>
 
-    <iframe src="footer.html" width="100%" height="300" frameborder="0" scrolling="no"></iframe>
-    
+    <?php include('footer.php'); ?>
 </body>
 </html>
